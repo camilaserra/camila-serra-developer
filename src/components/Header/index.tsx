a9844be4d09c06d28d4444378568cecfff075eca
Camila Serra <66450896+camilaserra@users.noreply.github.com>
2024-02-19 17:12:38 -0300

    Update logo image source in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -30,7 +30,7 @@ export function Header() {
         <div className="flex items-center justify-between">
           <Link href="/">
             <img
-              src="/img/logo.png"
+              src="/img/favicon-32x32.png"
               alt=""
               className="py-3 transition-all cursor-pointer"
               style={{ height: y > 0 ? '3rem' : '4rem' }}
